a9e66abb95dbc2668b7644280c2db4d12b47beb4
= <=>
2024-01-01 21:38:30 -0300

    added nvim-autopairs

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,11 @@
+return {
+  "windwp/nvim-autopairs",
+  setup = function()
+    require('nvim-autopairs').setup()
+  end,
+--  require("cmp").event:on(
+--    "confirm_done",
+--    require("nvim-autopairs.completion.cmp").on_confirm_done({
+--    })
+--  ),
+ }
