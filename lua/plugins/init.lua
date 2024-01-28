45de974ca3afcf063289010434877e04ee008a80
= <=>
2024-01-28 01:07:02 -0300

    added neodev configs recommended by nvim-dap-ui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,17 @@
 return {
 	"folke/which-key.nvim",
-	{"folke/neoconf.nvim", cmd="Neoconf"},
-	"folke/neodev.nvim",
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+  { "folke/neodev.nvim",
+    config = function()
+      require("neodev").setup({
+        library = {
+          plugins = {
+            "nvim-dap.ui"
+          },
+          types = true
+        },
+      })
+    end
+  },
+  lazy = false,
 }
